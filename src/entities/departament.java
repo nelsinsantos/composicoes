b262841e065dc96e320f3a64a7e7243eda4576e2
Nelsin Santos <nbatista1992@gmail.com>
2022-02-15 19:08:35 -0300

    Estudando composicoes

---
+++ src/entities/departament.java
@@ -0,0 +1,25 @@
+package entities;
+
+public class departament {
+	
+	private String name;
+	
+	public departament() {
+		
+	}
+
+	public departament(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+
+}
